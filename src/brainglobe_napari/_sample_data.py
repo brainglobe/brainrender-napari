ca3e16feb212af65b835b4d6bf00e7ad805f29b9
alessandrofelder <a.felder@ucl.ac.uk>
2023-02-27 15:46:52 +0000

    initial commit

---
+++ src/brainglobe_napari/_sample_data.py
@@ -0,0 +1,21 @@
+"""
+This module is an example of a barebones sample data provider for napari.
+
+It implements the "sample data" specification.
+see: https://napari.org/stable/plugins/guides.html?#sample-data
+
+Replace code below according to your needs.
+"""
+from __future__ import annotations
+
+import numpy
+
+
+def make_sample_data():
+    """Generates an image"""
+    # Return list of tuples
+    # [(data1, add_image_kwargs1), (data2, add_image_kwargs2)]
+    # Check the documentation for more information about the
+    # add_image_kwargs
+    # https://napari.org/stable/api/napari.Viewer.html#napari.Viewer.add_image
+    return [(numpy.random.rand(512, 512), {})]
---
+++ src/brainglobe_napari/_tests/test_sample_data.py
@@ -0,0 +1,7 @@
+# from brainglobe_napari import make_sample_data
+
+# add your tests here...
+
+
+def test_something():
+    pass
