91f7dadd7bd16f2d046b552544c288be1a7624b5
alessandrofelder <alessandrofelder@users.noreply.github.com>
2023-05-26 11:26:50 +0100

    document atlas viewer tests

--- src/brainglobe_napari/tests/test_atlas_viewer_widget.py
+++ src/brainglobe_napari/tests/test_atlas_viewer_widget.py
@@ -8,6 +8,9 @@ from brainglobe_napari.atlas_viewer_widget import AtlasViewerWidget
 
 @pytest.fixture
 def make_atlas_viewer(make_napari_viewer) -> Tuple[Viewer, AtlasViewerWidget]:
+    """Fixture to expose the atlas viewer widget to tests.
+    Simultaneously acts as a smoke test that the widget can
+    be instantiated without crashing."""
     viewer = make_napari_viewer()
     atlas_viewer = AtlasViewerWidget(viewer)
     return viewer, atlas_viewer
@@ -22,6 +25,8 @@ def make_atlas_viewer(make_napari_viewer) -> Tuple[Viewer, AtlasViewerWidget]:
     ],
 )
 def test_add_annotation_button(make_atlas_viewer, row, expected_atlas_name):
+    """Check for a few random atlas selections that clicking the
+    "Add Annotation" button adds a layer with the expected name."""
     viewer, atlas_viewer = make_atlas_viewer
 
     atlas_viewer.atlas_table_view.selectRow(row)
@@ -31,6 +36,8 @@ def test_add_annotation_button(make_atlas_viewer, row, expected_atlas_name):
 
 
 def test_add_annotations_button_no_selection(make_atlas_viewer):
+    """Check that clicking "Add Annotation" button without
+    a selection does not add a layer."""
     viewer, atlas_viewer = make_atlas_viewer
 
     atlas_viewer.add_annotation_button.click()
