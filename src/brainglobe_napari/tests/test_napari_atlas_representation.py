4b041dc9b631bb1562099f33ca2fe59d374cb941
alessandrofelder <a.felder@ucl.ac.uk>
2023-06-30 16:28:39 +0100

    add test for anisotropic atlases

--- src/brainglobe_napari/tests/test_napari_atlas_representation.py
+++ src/brainglobe_napari/tests/test_napari_atlas_representation.py
@@ -8,6 +8,7 @@ from brainglobe_napari.napari_atlas_representation import (
 )
 
 
+@pytest.mark.parametrize("anisotropic", [True, False])
 @pytest.mark.parametrize(
     "expected_atlas_name",
     [
@@ -16,13 +17,25 @@ from brainglobe_napari.napari_atlas_representation import (
         ("osten_mouse_100um"),
     ],
 )
-def test_add_to_viewer(make_napari_viewer, expected_atlas_name):
+def test_add_to_viewer(make_napari_viewer, expected_atlas_name, anisotropic):
     """Checks that calling add_to_viewer() adds the expected number of
     layers, of the expected type and with the expected name.
     Also checks that reference and annotation image have the same extents.
     """
     viewer = make_napari_viewer()
     atlas = BrainGlobeAtlas(atlas_name=expected_atlas_name)
+
+    if anisotropic:
+        # make atlas anisotropic
+        atlas.metadata["resolution"][0] *= 3
+        atlas.metadata["resolution"][1] *= 2
+        atlas._annotation = atlas.annotation[
+            0:len(atlas.annotation):3, 0:len(atlas.annotation[0]):2,:
+        ]
+        atlas._reference = atlas.reference[
+            0:len(atlas.reference):3, 0:len(atlas.reference[0]):2,:
+        ]
+
     atlas_representation = NapariAtlasRepresentation(atlas, viewer)
     atlas_representation.add_to_viewer()
     assert len(viewer.layers) == 3
