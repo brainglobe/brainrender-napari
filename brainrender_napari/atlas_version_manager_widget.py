6816696ed0ec8e70cdcd42f41a0c7bcf75ac3619
alessandrofelder <a.felder@ucl.ac.uk>
2023-09-21 15:44:05 +0100

    implement version manager widget

---
+++ brainrender_napari/atlas_version_manager_widget.py
@@ -0,0 +1,24 @@
+from qtpy.QtWidgets import QVBoxLayout, QWidget
+
+from brainrender_napari.widgets.atlas_manager_view import AtlasManagerView
+
+
+class AtlasVersionManagerWidget(QWidget):
+    def __init__(self):
+        """Instantiates the version manager widget
+        and sets up coordinating connections"""
+        super().__init__()
+
+        self.setLayout(QVBoxLayout())
+
+        # create widgets
+        self.atlas_manager_view = AtlasManagerView(parent=self)
+        self.layout().addWidget(self.atlas_manager_view)
+
+        self.atlas_manager_view.download_atlas_confirmed.connect(self._refresh)
+
+        self.atlas_manager_view.update_atlas_confirmed.connect(self._refresh)
+
+    def _refresh(self) -> None:
+        # refresh view once an atlas has been downloaded
+        self.atlas_manager_view = AtlasManagerView(parent=self)
