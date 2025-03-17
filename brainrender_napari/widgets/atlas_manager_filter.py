39e3993ad4a6c319fc996798fca5a5defac5fdfa
Farida Yusuf <frd.ysf@outlook.com>
2025-03-17 13:39:43 +0000

    Add checkbox to filter only downloaded atlases

--- brainrender_napari/widgets/atlas_manager_filter.py
+++ brainrender_napari/widgets/atlas_manager_filter.py
@@ -1,6 +1,16 @@
-from qtpy.QtWidgets import QComboBox, QHBoxLayout, QLabel, QLineEdit, QWidget
+from qtpy.QtWidgets import (
+    QCheckBox,
+    QComboBox,
+    QHBoxLayout,
+    QLabel,
+    QLineEdit,
+    QVBoxLayout,
+    QWidget,
+)
 
 from brainrender_napari.widgets.atlas_manager_view import AtlasManagerView
+
+# TODO: add tooltip to fields in filter widget
 
 
 class AtlasManagerFilter(QWidget):
@@ -18,16 +28,23 @@ class AtlasManagerFilter(QWidget):
 
     def setup_ui(self):
         """Creates embedded widgets and attaches these within a layout."""
-        self.layout = QHBoxLayout(self)
+        self.layout = QVBoxLayout(self)
         self.layout.setContentsMargins(0, 0, 0, 0)
         self.layout.setSpacing(0)
+
+        self.upper_row = QHBoxLayout()
+        self.layout.addLayout(self.upper_row)
+
+        self.lower_row = QHBoxLayout()
+        self.lower_row.setSpacing(10)
+        self.layout.addLayout(self.lower_row)
 
         self.query_field = QLineEdit(self)
         self.query_field.setPlaceholderText("Search...")
         self.query_field.textChanged.connect(self.apply)
 
-        self.layout.addWidget(QLabel("Query:"))
-        self.layout.addWidget(self.query_field)
+        self.upper_row.addWidget(QLabel("Query:"))
+        self.upper_row.addWidget(self.query_field)
 
         self.column_field = QComboBox()
         self.column_field.addItems(
@@ -39,8 +56,13 @@ class AtlasManagerFilter(QWidget):
         self.column_field.setCurrentIndex(0)
         self.column_field.currentIndexChanged.connect(self.apply)
 
-        self.layout.addWidget(QLabel("Column:"))
-        self.layout.addWidget(self.column_field)
+        self.lower_row.addWidget(QLabel("Column:"))
+        self.lower_row.addWidget(self.column_field)
+
+        self.downloaded_only_checkbox = QCheckBox("Downloaded only")
+        self.downloaded_only_checkbox.stateChanged.connect(self.apply)
+        self.lower_row.addWidget(self.downloaded_only_checkbox)
+
         return
 
     def clear(self):
@@ -58,6 +80,16 @@ class AtlasManagerFilter(QWidget):
         applies filter."""
         query = self.query_field.text()
         column = self.column_field.currentText()
+        downloaded_only = self.downloaded_only_checkbox.isChecked()
+
+        # TODO: if downloaded_only_checkbox is checked, filter
+        # only downloaded atlases, i.e. drop where local version = "n/a"
+
+        # needs custom filtering solution, see:
+        # https://www.dayofthenewdan.com/2013/02/09/Qt_QSortFilterProxyModel.html
+
+        if downloaded_only:
+            pass
 
         if column == "Any":
             self.atlas_manager_view.proxy_model.setFilterKeyColumn(-1)
