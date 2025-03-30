dc8e2f7857744468ff93f34c7727af4edd0af97d
Farida Yusuf <frd.ysf@outlook.com>
2025-03-30 20:11:03 +0100

    Create unit tests for AtlasManagerFilter

---
+++ tests/test_unit/test_atlas_manager_filter.py
@@ -0,0 +1,48 @@
+import pytest
+from brainglobe_atlasapi.list_atlases import get_all_atlases_lastversions
+
+from brainrender_napari.widgets.atlas_manager_view import AtlasManagerView
+
+all_atlases_names = get_all_atlases_lastversions().keys()
+all_atlases_versions = get_all_atlases_lastversions().values()
+
+
+@pytest.fixture
+def atlas_manager_view(qtbot):
+    return AtlasManagerView()
+
+
+def filter_data(query, data):
+    return list(filter(lambda row_attr: query in row_attr, data))
+
+
+def test_no_filter(atlas_manager_view):
+    atlas_manager_view.proxy_model.setFilterFixedString("")
+    assert atlas_manager_view.proxy_model.rowCount() == len(all_atlases_names)
+
+
+@pytest.mark.parametrize(
+    "query, data, column_name",
+    [
+        ("mouse", all_atlases_names, "Any"),
+        (
+            "50",
+            all_atlases_names,
+            "Atlas",
+        ),  # 50um or 500um is in the atlas name
+        ("1.2", all_atlases_versions, "Latest version"),
+    ],
+)
+def test_filter_query(atlas_manager_view, query, data, column_name):
+    filtered_data = filter_data(query, data)
+
+    if column_name == "Any":
+        column_index = -1
+    else:
+        column_index = atlas_manager_view.source_model.column_headers.index(
+            column_name
+        )
+
+    atlas_manager_view.proxy_model.setFilterFixedString(query)
+    atlas_manager_view.proxy_model.setFilterKeyColumn(column_index)
+    assert atlas_manager_view.proxy_model.rowCount() == len(filtered_data)
