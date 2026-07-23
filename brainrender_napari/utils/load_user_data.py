f1557a4889717523f662ca1b83d2408c72e15e88
Igor Tatarnikov <igor.tatarnikov@gmail.com>
2026-07-23 16:47:53 +0100

    Use the api to fetch metadata and structure information

--- brainrender_napari/utils/load_user_data.py
+++ brainrender_napari/utils/load_user_data.py
@@ -1,26 +1,39 @@
-import json
+from functools import lru_cache
 
-from brainglobe_atlasapi import config
-from brainglobe_atlasapi.list_atlases import get_local_atlas_version
+from brainglobe_atlasapi import BrainGlobeAtlas
 
 
+@lru_cache(maxsize=32)
 def read_atlas_metadata_from_file(atlas_name: str):
-    """Reads atlas metadata stored in a `.json` in the BrainGlobe directory."""
-    brainglobe_dir = config.get_brainglobe_dir()
-    with open(
-        brainglobe_dir
-        / f"{atlas_name}_v{get_local_atlas_version(atlas_name=atlas_name)}"
-        / "metadata.json",
-    ) as metadata_file:
-        return json.loads(metadata_file.read())
+    """Reads atlas metadata from the local manifest in the BrainGlobe
+    directory.
+    """
+    atlas = BrainGlobeAtlas(atlas_name=atlas_name, check_latest=False)
+
+    keys_to_remove = [
+        "coordinate_space",
+        "terminology",
+        "annotation_set",
+        "template",
+    ]
+
+    metadata_dict = atlas.metadata
+    metadata_dict = {
+        k: v for k, v in metadata_dict.items() if k not in keys_to_remove
+    }
+    additional_references = metadata_dict.get("additional_references", [])
+    if additional_references:
+        additional_references = [ref["name"] for ref in additional_references]
+        metadata_dict["additional_references"] = additional_references
+
+    return metadata_dict
 
 
+@lru_cache(maxsize=32)
 def read_atlas_structures_from_file(atlas_name: str):
-    """Reads structure info from a '.json' in the BrainGlobe directory."""
-    brainglobe_dir = config.get_brainglobe_dir()
-    with open(
-        brainglobe_dir
-        / f"{atlas_name}_v{get_local_atlas_version(atlas_name=atlas_name)}"
-        / "structures.json",
-    ) as metadata_file:
-        return json.loads(metadata_file.read())
+    """Reads atlas structure info from the local terminology file in the
+    BrainGlobe directory.
+    """
+    atlas = BrainGlobeAtlas(atlas_name=atlas_name, check_latest=False)
+
+    return atlas.structures_list
